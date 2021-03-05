eba50c6d4e0921a31d44ccf2516fe1cc9d4de9f2
Ajakaiye Afeso <marcusitovie@gmail.com>
2021-03-05 20:37:25 +0100

    Add initial file

---
+++ script.js
@@ -0,0 +1,13 @@
+(function() {
+	const displayText = document.querySelector(".display-text");
+	const input = document.querySelector(".text");
+	const btn = document.querySelector(".btn");
+
+	btn.addEventListener("click", function() {
+		if (input.value) {
+			displayText.classList.add("display");
+			displayText.textContent = input.value;
+		}
+	});
+})()
+
