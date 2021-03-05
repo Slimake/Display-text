b949039ad80594cf4adc97809ce12c95a9c6348a
Ajakaiye Afeso <marcusitovie@gmail.com>
2021-03-05 21:52:46 +0100

    Modify initial file

--- script.js
+++ script.js
@@ -3,11 +3,21 @@
 	const input = document.querySelector(".text");
 	const btn = document.querySelector(".btn");
 
-	btn.addEventListener("click", function() {
+	function show() {
 		if (input.value) {
 			displayText.classList.add("display");
 			displayText.textContent = input.value;
 			input.value = "";
+		}
+	}
+
+	btn.addEventListener("click", function() {
+		show();
+	});
+
+	input.addEventListener("keydown", function(e) {
+		if (e.keyCode === 13) {
+			show();
 		}
 	});
 })()
